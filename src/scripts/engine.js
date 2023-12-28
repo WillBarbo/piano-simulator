026e3150becdea3cb10e2cfa00774c1df42b42e9
Willian Barbosa <willianbsilva96@gmail.com>
2023-12-28 18:51:28 -0300

    add audio piano keys

---
+++ src/scripts/engine.js
@@ -0,0 +1,14 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key")
+
+let audio = new Audio("src/tunes/a.wav")
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`
+  audio.play()
+}
+
+pianoKeys.forEach((key) => {
+  console.log(key.dataset.key) //1° key -> parameter, dataset -> "data-key", 2° key -> valor da key no HTML
+  key.addEventListener("click", () => playTune(key.dataset.key)) //sem "{}", caso seja somente uma linha
+})
+
