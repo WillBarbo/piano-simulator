4c28a564f2da0b00b3efe2a4d479192a2beb4ab4
Willian Barbosa <willianbsilva96@gmail.com>
2024-01-02 19:06:36 -0300

    allow only selected keyboard keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key")
+const volumeSlider = document.querySelector(".volume-slider input")
 
+let mapedKeys = []
 let audio = new Audio("src/tunes/a.wav")
 
 const playTune = (key) => {
@@ -14,10 +16,13 @@ const playTune = (key) => {
 }
 
 pianoKeys.forEach((key) => {
-  console.log(key.dataset.key) //1° key -> parameter, dataset -> "data-key", 2° key -> valor da key no HTML
+  //console.log(key.dataset.key) 1° key -> parameter, dataset -> "data-key", 2° key -> valor da key no HTML
   key.addEventListener("click", () => playTune(key.dataset.key)) //sem "{}", caso seja somente uma linha
+  mapedKeys.push(key.dataset.key)
 })
 
 document.addEventListener("keydown", (e) => {
-  playTune(e.key)
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key)
+  }
 })
